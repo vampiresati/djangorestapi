72c54ce7c515eca0a980c2ecd0a7c9a097a9917c
satvir singh <sssatvir12@gmail.com>
2022-07-02 15:02:28 +0530

    Basic Authentication using

--- authapi/authapi/settings.py
+++ authapi/authapi/settings.py
@@ -131,6 +131,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     )
 }
--- authapi/firstapi/views.py
+++ authapi/firstapi/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 class FirstView(APIView):
     permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
     def get(self, request, *args, **kwargs):
         return Response({'data':'helllo how are you'})
